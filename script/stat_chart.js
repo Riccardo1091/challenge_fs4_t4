5b1555ac574a9c6cbac48ae94211b90e04753856
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-08-02 18:34:59 +0200

    grafici attivi

--- script/stat_chart.js
+++ script/stat_chart.js
@@ -4,10 +4,10 @@ let ctx2 = document.getElementById('chartMonths').getContext('2d');
 
 // setup 
 const dataChartNewsSite = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Extra','Extra2'],
+    labels: [],
     datasets: [{
         label: 'Articolo',
-        data: [10, 10, 20, 10, 10, 20, 5, 5, 10],
+        data: [],
         backgroundColor: [
         'rgba(255, 26, 104, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -32,10 +32,10 @@ const dataChartNewsSite = {
 };
 
 const dataChartMonths = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    labels: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
     datasets: [{
         label: 'Articolo',
-        data: [10, 10, 20, 10, 20, 20, 10],
+        data: [],
         backgroundColor: [
         'rgba(255, 26, 104, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -52,7 +52,13 @@ const dataChartMonths = {
         'rgba(75, 192, 192, 1)',
         'rgba(153, 102, 255, 1)',
         'rgba(255, 159, 64, 1)',
-        'rgba(0, 0, 0, 1)'
+        'rgba(0, 0, 0, 1)',
+        'rgba(255, 26, 104, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
         ],
         borderWidth: 2,
         hoverOffset: 10
@@ -104,13 +110,13 @@ const configChartMonths = {
         responsive: true,
         layout: {},
         plugins: {
-            datalabels: {
-                formatter: function(value, context) {
-                    return context.chart.data.labels[context.dataIndex];
-                }
-            },
+            // datalabels: {
+            //     formatter: function(value, context) {
+            //         return context.chart.data.labels[context.dataIndex];
+            //     }
+            // },
             labels: {
-                render: 'percentage',
+                render: 'linear',
                 fontColor: dataChartMonths.datasets[0].borderColor,
                 fontStyle: 'bolder',
                 position:'outside',
@@ -137,3 +143,46 @@ const configChartMonths = {
 // avvio chart
 const chartNewsSite = new Chart(document.getElementById('chartNewsSite'), configChartNewsSite);
 const chartMonths = new Chart(document.getElementById('chartMonths'), configChartMonths);
+
+// primo grafico - percentuale articoli per autore per fetch
+let pubblications = null;
+let autori = [];
+(async () => {
+    const response = await fetch("https://api.spaceflightnewsapi.net/v3/articles");
+    pubblications = await response.json();
+    pubblications.forEach( pubblication => {  //creo array di autori
+        if (autori.indexOf(pubblication.newsSite) === -1) {
+            autori.push(pubblication.newsSite);
+        }
+    })
+    dataChartNewsSite.labels = autori
+
+    autori.forEach(autore => {
+        let counter = 0
+        pubblications.forEach(pub => {
+            if (pub.newsSite == autore)
+                counter++
+        })
+        const rapporto = counter/pubblications.length*100
+        dataChartNewsSite.datasets[0].data.push(rapporto)   
+    })
+    chartNewsSite.update()
+})();
+
+// secondo grafico - numero articoli per mese per autore
+(async () => {
+    let mesi = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    for (mese of mesi) {
+        const res = await fetch(`https://api.spaceflightnewsapi.net/v3/articles/count?publishedAt_gt=2021-${mese}&newsSite_contains=2`);
+        let num_art = await res.json()
+        dataChartMonths.datasets[0].data.push(num_art)
+    }
+    chartMonths.update()
+})();
+
+// let grafici = document.querySelectorAll('canvas')
+// for (let graf of grafici)
+//     graf.addEventListener('click', (e) => {
+//         chartNewsSite.update()
+//         chartMonths.update()
+// })
